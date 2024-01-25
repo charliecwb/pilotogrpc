dba6bf35c971d91faa5bc1fe5336e530d13ad482
charliecwb <charliecwb@gmail.com>
2024-01-25 16:00:04 -0300

    fixed handlers

--- internal/services/handlers.go
+++ internal/services/handlers.go
@@ -1,17 +1,11 @@
 package services
 
-import (
-	"context"
-	"fmt"
-	pb "pilotoGRPC/pkg/pb/greeting/v1"
-)
+//type GreetService struct {
+//}
 
-type GreetService struct {
-}
-
-func (s *GreetService) Greet(ctx context.Context, req *pb.GreetRequest) (*pb.GreetResponse, error) {
-	return &pb.GreetResponse{
-		Resp: fmt.Sprintf("%s, %s", req.Msg.Greeting.String(), req.Msg.Name),
-	}, nil
-
-}
+//func (s *GreetService) Greet(ctx context.Context, req *pb.GreetRequest) (*pb.GreetResponse, error) {
+//	return &pb.GreetResponse{
+//		Resp: fmt.Sprintf("%s, %s", req.Msg.Greeting.String(), req.Msg.Name),
+//	}, nil
+//
+//}
